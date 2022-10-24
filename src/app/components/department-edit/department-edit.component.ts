c28c0482aaaefde4269bbcdd88ed5c51fc4ccc8d
Fhellipe Valentin <fhellipereis@outlook.com>
2022-10-24 18:17:29 -0300

    employee e department edit implementados corretamente

--- src/app/components/department-edit/department-edit.component.ts
+++ src/app/components/department-edit/department-edit.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { DepartmentService } from 'src/app/services/department.service';
 
 @Component({
   selector: 'app-department-edit',
@@ -7,9 +10,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DepartmentEditComponent implements OnInit {
 
-  constructor() { }
-
+  constructor( 
+    private departmentService: DepartmentService,
+    private activatedRoute: ActivatedRoute,
+    private route: Router,
+    private formBuilder: FormBuilder ) { }
+  
+  formulary!: FormGroup;
+  id = this.activatedRoute.snapshot.params['id']
+  updateData: any = {}
+  
   ngOnInit(): void {
+    const id = this.activatedRoute.snapshot.params['id']
+    this.departmentService
+      .accessDataDepartmentById(id)
+      .subscribe((data) => {
+        this.updateData = data;/*
+        this.updateData.created_At = new Date().toLocaleTimeString() + ' ' + new Date().toLocaleDateString()*/
+      });
+      this.formulary = this.formBuilder.group( {
+        categoria: new FormControl('', Validators.required)
+      })
   }
+  
+  updatingData() {
+    if (confirm('Are you want to edit the task?')) {
+      this.departmentService.updateDepartment(this.id, this.updateData).subscribe(()=>{
+        this.route.navigate(['/home'])
+      })
+    }
+  }
+
 
 }
--- src/app/components/employee-edit/employee-edit.component.ts
+++ src/app/components/employee-edit/employee-edit.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
+import { DepartmentService } from 'src/app/services/department.service';
 import { EmployeeService } from '../../services/employee.service';
 
 @Component({
@@ -12,29 +13,35 @@ export class EmployeeEditComponent implements OnInit {
 
   constructor( 
     private employeeService: EmployeeService,
+    private departmentService: DepartmentService,
     private activatedRoute: ActivatedRoute,
     private route: Router,
     private formBuilder: FormBuilder ) { }
   
   formulary!: FormGroup;
   id = this.activatedRoute.snapshot.params['id']
-  updateData: any = {
-  }
+  updateData: any = {}
   
+  departmentList: any = []
+
   ngOnInit(): void {
     const id = this.activatedRoute.snapshot.params['id']
     this.employeeService
       .accessDataEmployeeById(id)
       .subscribe((data) => {
-        /*this.updateData = data;
+        this.updateData = data;/*
         this.updateData.created_At = new Date().toLocaleTimeString() + ' ' + new Date().toLocaleDateString()*/
       });
       this.formulary = this.formBuilder.group( {
         name: new FormControl('', Validators.required),
-        email: new FormControl('', Validators.required),
+        email: new FormControl('', [Validators.required, Validators.email]),
         phone: new FormControl('', Validators.required),
         department: new FormControl('', Validators.required)
       })
+    
+    this.departmentService.listDataDepartment().subscribe((data: {})=> {
+      this.departmentList = data
+    })
   }
   
   updatingData() {
